4827d2550fec249ec6a4c16f2e967ce0c30b85e0
taysa-fernandes <taysamirelly34@gmail.com>
2023-10-07 10:54:09 -0300

    atividade 7

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Sebo_discos
+from .models import Sebo_discos,selo_fonografico,artista
 
 admin.site.register(Sebo_discos)
+admin.site.register(selo_fonografico)
+admin.site.register(artista)
--- core/models.py
+++ core/models.py
@@ -4,11 +4,21 @@ genero_choices= (
     ("t","terror"),
     ("d","drama")
 )
-
+class selo_fonografico(models.Model):
+    nome = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nome
+    
+class artista(models.Model):
+    nome = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nome
 class Sebo_discos(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField()
-    selo_fonografico = models.CharField(max_length=200)
+    selo_fonografico = models.ForeignKey(selo_fonografico,on_delete=models.CASCADE)
     ano = models.IntegerField()
     pais = models.CharField(max_length=20)
     genero = models.CharField(
@@ -16,5 +26,7 @@ class Sebo_discos(models.Model):
         choices=genero_choices
     )
     quantidade = models.IntegerField()
+    artistas = models.ManyToManyField(artista)
+    
     def __str__(self):
         return self.nome
